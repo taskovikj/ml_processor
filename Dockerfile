353c7e98fecd07782cbaea8e29a0d01d9bd22131
Branislav <branislav.taskovikj@gmail.com>
2023-07-25 09:41:12 +0200

    initial

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-# Dockerfile for ml_processor
+# Use the official Python base image
 FROM python:3.8
 
 # Set the working directory inside the container
@@ -8,8 +8,7 @@ WORKDIR /app
 COPY . /app
 
 # Install project dependencies
-COPY requirements.txt /app/
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir Flask gunicorn celery[redis]
 
-# Command to run your Python application (assuming your Flask app is named "app.py")
-CMD ["gunicorn", "app:app", "-b", "0.0.0.0:8001"]
+# Command to run your Python application
+CMD [ "gunicorn", "app:app", "-b", "0.0.0.0:8001" ]
--- app.py
+++ app.py
@@ -1,20 +1,36 @@
-# ml_processor/app.py
-
 from flask import Flask, request, jsonify
+from celery import Celery
+import time
 
 app = Flask(__name__)
 
+# Celery configuration
+app.config['CELERY_BROKER_URL'] = 'redis://redis:6380/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://redis:6380/0'
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+
+# Celery task function
+@celery.task
+def process_string_task(input_string):
+    # Simulating some processing delay
+    time.sleep(5)
+    # Your processing logic here (replace this with your actual processing code)
+    return input_string.upper()
+
+
+# Flask route for processing the string
 @app.route('/process_string', methods=['POST'])
 def process_string():
     data = request.get_json()
     input_string = data['input_string']
 
-    # Process the input_string using your ml_processor functions
-    processed_result = ml_processor_function(input_string)
+    # Send the input_string to Celery for processing asynchronously
+    result = process_string_task.delay(input_string)
 
-    return jsonify({'result': processed_result})
+    return jsonify({'task_id': result.id}), 202  # Return task ID and status 202 Accepted
 
-# Define your ml_processor_function here
-def ml_processor_function(input_string):
-    # Your implementation for processing the string goes here
-    return input_string.upper()  # Exa convert input string to uppercasemple:
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8001)
