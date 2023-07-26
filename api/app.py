c79a41c8be6d53f65edf3dbf4a4a60af772c0f95
Branislav <branislav.taskovikj@gmail.com>
2023-07-26 12:51:27 +0200

    day 3 final

--- api/app.py
+++ api/app.py
@@ -1,10 +1,22 @@
 import celery.states as states
 from flask import Flask, Response
 from flask import url_for, jsonify
-from api.worker import celery
+from worker import celery
+from flask import Flask, jsonify, request
+
+
 
 dev_mode = True
 app = Flask(__name__)
+
+
+@app.route('/process_data', methods=['POST'])
+def process_data():
+    data = request.json['data']
+
+    result = data.upper()
+
+    return jsonify(result), 200
 
 
 @app.route('/add/<int:param1>/<int:param2>')
@@ -30,3 +42,11 @@ def health_check() -> Response:
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
+
+
+@app.route('/enqueue-ml-task')
+def enqueue_ml_task():
+    data = "Some data for the ML task"
+    from celery_queue.tasks import run_ml_task
+    result = run_ml_task.apply_async(args=[data], queue='ml_queue')
+    return f"ML Task enqueued: {result.id}"
--- celery_queue/tasks.py
+++ celery_queue/tasks.py
@@ -1,15 +1,31 @@
 import os
 import time
-from celery import Celery
-
+from celery import Celery, shared_task
 
 CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379'),
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
 
 celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+app = Celery('myapp', broker='redis://localhost:6379/0', backend='redis://localhost:6379/1')
 
 
 @celery.task(name='tasks.add')
 def add(x: int, y: int) -> int:
     time.sleep(5)
     return x + y
+
+
+@app.task
+def my_flask_task(param1, param2):
+    time.sleep(5)
+    return param1 + param2
+
+
+
+@app.task(queue='ml_queue')
+@shared_task
+def run_ml_task(data):
+    result = "ML Task Result: " + data
+    print(result)
+    return result
+
